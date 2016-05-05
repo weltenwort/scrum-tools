963724ee209955a69b50f393ecb78f481ca0f5a6
Felix Stürmer <stuermer@weltenwort.de>
2016-05-05 18:08:40 +0200

    Reformat to match eslint style

--- server/index.js
+++ server/index.js
@@ -1,53 +1,54 @@
-const bodyParser = require('body-parser');
-const configuration = require('feathers-configuration');
-const createDebug = require('debug');
-const feathers = require('feathers');
-const feathersNedb = require('feathers-nedb');
-const morgan = require('morgan');
-const NeDB = require('nedb');
-const socketio = require('feathers-socketio');
+import path from 'path';
 
+import bodyParser from 'body-parser';
+import configuration from 'feathers-configuration';
+import createDebug from 'debug';
+import feathers from 'feathers';
+import feathersNedb from 'feathers-nedb';
+import morgan from 'morgan';
+import NeDB from 'nedb';
+import socketio from 'feathers-socketio';
 
-const CONFIG_DIRECTORY = (!!process.env.ST_CONFIG_DIRECTORY
-    ? process.env.ST_CONFIG_DIRECTORY
-    : __dirname + "/.."
+const CONFIG_DIRECTORY = (process.env.ST_CONFIG_DIRECTORY
+  ? process.env.ST_CONFIG_DIRECTORY
+  : path.join(__dirname, '..')
 );
-const isProductionEnv = (app) => app.get('env') === 'production'
+const isProductionEnv = (app) => app.get('env') === 'production';
 
 const app = feathers()
-    .configure(configuration(CONFIG_DIRECTORY))
-    .configure(socketio())
-    .use(morgan('combined'))
-    .use(bodyParser.json());
+  .configure(configuration(CONFIG_DIRECTORY))
+  .configure(socketio())
+  .use(morgan('combined'))
+  .use(bodyParser.json());
 
 if (!isProductionEnv(app)) {
-    createDebug.enable('scrumTools:*');
+  createDebug.enable('scrumTools:*');
 }
 
-const logHttp = createDebug('scrumTools:http')
+const logHttp = createDebug('scrumTools:http');
 
 app.use('/retrospectives', feathersNedb({
-    Model: new NeDB({
-        filename: app.get('retrospectives_db'),
-        autoload: true
-    })
+  Model: new NeDB({
+    filename: app.get('retrospectives_db'),
+    autoload: true,
+  }),
 }));
 
 if (!isProductionEnv(app)) {
-    app.use('/', feathers.static(app.get('assets_directory')));
+  app.use('/', feathers.static(app.get('assets_directory')));
 }
 
 const server = app.listen(
-    app.get('port'),
-    app.get('address'),
-    () => {
-        const addressInfo = server.address();
-        const address = (addressInfo.family === 'IPv6'
-            ? `[${addressInfo.address}]`
-            : addressInfo.address
-        );
-        const port = addressInfo.port;
+  app.get('port'),
+  app.get('address'),
+  () => {
+    const addressInfo = server.address();
+    const address = (addressInfo.family === 'IPv6';
+      ? `[${addressInfo.address}]`
+      : addressInfo.address
+    );
+    const port = addressInfo.port;
 
-        logHttp(`Server listening on http://${address}:${port}`)
-    }
+    logHttp(`Server listening on http://${address}:${port}`);
+  }
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-var webpack = require('webpack');
-var merge = require('webpack-merge');
-var autoprefixer = require('autoprefixer');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable no-console */
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // detemine build env
-var TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'prod' : 'dev';
+const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'prod' : 'dev';
 
 // common webpack config
-var commonConfig = {
+const commonConfig = {
   entry: './src/index.js',
 
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
   },
 
   resolve: {
     modulesDirectories: ['node_modules'],
-    extensions: ['', '.js']
+    extensions: ['', '.js'],
   },
 
   module: {
@@ -28,22 +29,22 @@ var commonConfig = {
         exclude: [/node_modules/],
         loader: 'babel',
         query: {
-          presets: ['es2015']
-        }
+          presets: ['es2015'],
+        },
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'file'
+        loader: 'file',
       },
       {
         test: /\.(json)$/,
-        loader: 'json'
-      }
+        loader: 'json',
+      },
     ],
 
     noParse: [
-      /socket.io-client\/socket.io.js$/
-    ]
+      /socket.io-client\/socket.io.js$/,
+    ],
   },
 
   plugins: [
@@ -55,22 +56,22 @@ var commonConfig = {
       title: 'Scrum Tools',
       window: {
         env: {
-          appMountId: 'main'
-        }
-      }
-    })
-  ]
-}
+          appMountId: 'main',
+        },
+      },
+    }),
+  ],
+};
 
 // additional webpack settings for local env (when invoked by 'npm start')
-if (TARGET_ENV === 'dev' ) {
+if (TARGET_ENV === 'dev') {
   console.log('Building for development...');
 
   module.exports = merge(commonConfig, {
 
     devServer: {
       inline: true,
-      stats: 'errors-only'
+      stats: 'errors-only',
     },
 
     module: {
@@ -81,22 +82,22 @@ if (TARGET_ENV === 'dev' ) {
             'style-loader',
             'css-loader',
             'postcss-loader',
-            'sass-loader'
-          ]
-        }
-      ]
+            'sass-loader',
+          ],
+        },
+      ],
     },
 
     postcss: [
       autoprefixer({
-        browsers: ['last 2 versions']
-      })
-    ]
+        browsers: ['last 2 versions'],
+      }),
+    ],
   });
 }
 
 // additional webpack settings for prod env (when invoked via 'npm run build')
-if (TARGET_ENV === 'prod' ) {
+if (TARGET_ENV === 'prod') {
   console.log('Building for production...');
 
   module.exports = merge(commonConfig, {
@@ -107,30 +108,32 @@ if (TARGET_ENV === 'prod' ) {
           loader: ExtractTextPlugin.extract('style-loader', [
             'css-loader',
             'postcss-loader',
-            'sass-loader'
-          ])
-        }
-      ]
+            'sass-loader',
+          ]),
+        },
+      ],
     },
 
     postcss: [
       autoprefixer({
-        browsers: ['last 2 versions']
-      })
+        browsers: ['last 2 versions'],
+      }),
     ],
 
     plugins: [
       // extract CSS into a separate file
-      new ExtractTextPlugin('./css/stylesheet.css', {allChunks: true}),
+      new ExtractTextPlugin('./css/stylesheet.css', {
+        allChunks: true,
+      }),
 
       // minify & mangle JS/CSS
       new webpack.optimize.UglifyJsPlugin({
         minimize: true,
         compressor: {
-          warnings: false
+          warnings: false,
         },
-        mangle: true
-      })
-    ]
+        mangle: true,
+      }),
+    ],
   });
 }
